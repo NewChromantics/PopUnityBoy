d471e074b8d7bdebfada6f519d3eb6fd2f087f43
Graham Reeves <graham@grahamreeves.com>
2017-10-17 18:45:49 +0100

    Added threaded rendering

--- Assets/GameboyManager.cs
+++ Assets/GameboyManager.cs
@@ -1,6 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UnityEvent_Texture : UnityEvent <Texture> {}
+
 
 public class GameboyManager : MonoBehaviour {
 
@@ -11,6 +17,7 @@ public class GameboyManager : MonoBehaviour {
 	GarboDev.Memory			Memory;
 	public RenderTexture	FrameTarget;
 	Texture2D				Frame2D;
+	public UnityEvent_Texture	OnTextureUpdated;
 	public bool				Flip = true;
 	public bool				ForceOpaque = true;
 
@@ -18,10 +25,16 @@ public class GameboyManager : MonoBehaviour {
 	public float			TimeScalar = 1;
 	float?					StartTime = null;
 
+	[Range(100,160*240)]
+	public int				WritePixelsPerThread = 100;
+	[Range(1,160*240)]
+	public int				PixelCountClip = 160*240;
+
+
 	void Start ()
 	{
-		Debug.Log("Start");
-		Graphics.Blit (Texture2D.whiteTexture, FrameTarget);
+		if ( FrameTarget != null )
+			Graphics.Blit (Texture2D.whiteTexture, FrameTarget);
 
 		Memory = new GarboDev.Memory ();
 
@@ -43,7 +56,6 @@ public class GameboyManager : MonoBehaviour {
 		GbaManager.StartEmulator ( Renderer );
 		GbaManager.LoadRom (RomBytes);
 		GbaManager.Resume ();
-		Debug.Log("Start finished");
 	}
 
 	float GetSystemTimeSecs()
@@ -61,48 +73,102 @@ public class GameboyManager : MonoBehaviour {
 		StartTime = null;
 	}
 
+
+	unsafe void ThreadedCopy(uint* Frame,Color* Pixels,int Frame2D_width,int Frame2D_height,int FrameArray_Length,int PixelsArray_Length)
+	{
+		int PixelsDrawn = 0;
+		int PixelsQueued = 0;
+
+		bool Aborted = false;
+		System.Action<int,int> CopyPixels = (PixelIndex, PixelCount) => 
+		{
+			try
+			{
+				var rgba = new Color ();
+
+				for (int p = PixelIndex;	p <PixelIndex+PixelCount;	p++) {
+
+					var x = p % Frame2D_width;
+					var y = p / Frame2D_width;
+
+					if (Flip)
+						y = Frame2D_height -1 - y;
+
+					var FrameIndex = x + (y * Frame2D_width);
+					var rgba32 = Frame [FrameIndex];
+
+					var a = (rgba32 >> 24) & 0xff;
+					var r = (rgba32 >> 16) & 0xff;
+					var g = (rgba32 >> 8) & 0xff;
+					var b = (rgba32 >> 0) & 0xff;
+
+					if ( ForceOpaque )
+						a = 255;
+
+					rgba.r = r / 255.0f;
+					rgba.g = g / 255.0f;
+					rgba.b = b / 255.0f;
+					rgba.a = a / 255.0f;
+					Pixels [p] = rgba;
+				}
+			}
+			catch
+			{
+				Aborted = true;
+			}
+
+			Interlocked.Add( ref PixelsDrawn, PixelCount );
+		};
+
+
+		var TotalPixels = Mathf.Min (PixelCountClip, Mathf.Min (FrameArray_Length, PixelsArray_Length));
+		int ThreadCount = TotalPixels / WritePixelsPerThread;
+		for (int t = 0;	t < ThreadCount+1;	t++) {
+			int FirstPixel = WritePixelsPerThread * t;
+			int PixelsToDraw = WritePixelsPerThread;
+
+			if (FirstPixel + PixelsToDraw > TotalPixels)
+				PixelsToDraw = TotalPixels - FirstPixel;
+
+			PixelsQueued += PixelsToDraw;
+
+			if ( PixelsToDraw > 0 )
+				ThreadPool.QueueUserWorkItem ( (x) =>	{	CopyPixels (FirstPixel, PixelsToDraw);	}	);
+		}
+
+		while (PixelsDrawn < PixelsQueued && !Aborted ) {
+			//Debug.Log ("Waiting for " + (PixelsQueued - PixelsDrawn) + " pixels to draw");
+			Thread.Sleep(1);
+		}
+	}
+		
+
+
 	void Update () 
 	{
-		if (GbaManager == null)
-			Debug.Log ("Gbamanager is null");
-		
 		GbaManager.EmulatorIteration ();
 
 		var renderer = Renderer as GarboDev.Renderer;
-		var Pixels = Frame2D.GetPixels ();
-		var Frame = renderer.GetFrame ();
+		var PixelsArray = Frame2D.GetPixels ();
+		var FrameArray = renderer.GetFrame ();
 
+		unsafe
 		{
-			var rgba = new Color ();
-			for (int p = 0;	p < Mathf.Min (Frame.Length, Pixels.Length);	p++) {
-
-				var x = p % Frame2D.width;
-				var y = p / Frame2D.width;
-
-				if (Flip)
-					y = Frame2D.height -1 - y;
-
-				var FrameIndex = x + (y * Frame2D.width);
-				var rgba32 = Frame [FrameIndex];
-
-				var a = (rgba32 >> 24) & 0xff;
-				var r = (rgba32 >> 16) & 0xff;
-				var g = (rgba32 >> 8) & 0xff;
-				var b = (rgba32 >> 0) & 0xff;
-
-				if ( ForceOpaque )
-					a = 255;
-
-				rgba.r = r / 255.0f;
-				rgba.g = g / 255.0f;
-				rgba.b = b / 255.0f;
-				rgba.a = a / 255.0f;
-				Pixels [p] = rgba;
+			fixed(uint* Frame = &FrameArray[0])
+			{
+				fixed(Color* Pixels = &PixelsArray[0])
+				{
+					ThreadedCopy( Frame, Pixels, Frame2D.width, Frame2D.height, FrameArray.Length, PixelsArray.Length );
+				}
 			}
 		}
-		Frame2D.SetPixels (Pixels);
-		Frame2D.Apply ();
-		Graphics.Blit (Frame2D, FrameTarget);
 
+		Frame2D.SetPixels (PixelsArray);
+		Frame2D.Apply ();
+
+		if ( FrameTarget != null )
+			Graphics.Blit (Frame2D, FrameTarget);
+
+		OnTextureUpdated.Invoke (Frame2D);
 	}
 }
